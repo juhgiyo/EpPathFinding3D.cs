0a3fc9d2ed2e744614586f43f9048a39bac1425e
Woong Gyu La <juhgiyo@gmail.com>
2017-04-20 15:35:43 +0900

    project updated

--- EpPathFinding3D.cs/EpPathFinding3D.cs/Properties/AssemblyInfo.cs
+++ EpPathFinding3D.cs/EpPathFinding3D.cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EpPathFinding.cs")]
+[assembly: AssemblyTitle("EpPathFinding3D.cs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EpPathFinding.cs")]
+[assembly: AssemblyProduct("EpPathFinding3D.cs")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
